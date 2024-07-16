d2327efe1cb1e81a75354f9a38e5e292253a4a86
sohrab <syusibov92@gmail.com>
2024-07-16 06:28:02 -0700

    333

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(argc))
+        for i in range(1, argc + 1) :
+            print("{}: {}".format(i, argv[1]))   
