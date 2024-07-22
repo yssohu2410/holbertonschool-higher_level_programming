0f01e8df2d9f90c80f3ad8d74ffb526c6a0a275c
sohrab <syusibov92@gmail.com>
2024-07-22 10:23:26 -0700

    333

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0 and idx >= len(my_list):
+        return my_list
+    else:
+        x = my_list[idx]
+        my_list.remove(x)
+        my_list.insert(idx, element)
+        return my_list
