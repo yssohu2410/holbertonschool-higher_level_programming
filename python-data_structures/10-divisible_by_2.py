14d7878d2dde7e78d892a44dacf80823c80206b2
sohrab <syusibov92@gmail.com>
2024-07-23 11:52:37 -0700

    44

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3 
+def divisible_by_2(my_list=[]):
+    listt = []
+    for i in range(0, len(my_list)):
+        if my_list[i] % 2 == 0:
+            listt.append(True)
+        else:
+            listt.append(False)
+    return listt
+        
