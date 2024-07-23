4d86508f288fae7c426731364618e17e1ac92bc7
sohrab <syusibov92@gmail.com>
2024-07-23 11:08:55 -0700

    44

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if(sentence) == 0:
+    if len(sentence) == 0:
         return(0, None)
     else:
         result = (len(sentence), sentence[0])
