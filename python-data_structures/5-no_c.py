a79d813379b75d25d021fde60f30763b6aa70f5e
sohrab <syusibov92@gmail.com>
2024-07-23 07:11:59 -0700

    333

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     string = ""
     for i in my_string:
-        if i != 'C' and i != 'c':
+        if i != "C" and i != "c":
             string = string + i
     return string
